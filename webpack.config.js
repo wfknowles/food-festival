1a8a057cccad77184db07834505692ea7840d722
Will Knowles <will@willknowles.com>
2021-10-29 11:54:56 -0500

    file-loader and image-webpack-loader added

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,54 @@
-const path = require('path');
-const webpack = require("webpack");
+// npm i -D webpack-bundle-analyzer
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const webpack = require("webpack")
+const path = require("path");
+
 module.exports = {
-  entry: './assets/js/script.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.bundle.js'
-  },
-  plugins:[
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery"
-    }),
-  ],
-  mode: 'development'
+    entry: {
+        // chunked assets
+        app: "./assets/js/script.js",
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: __dirname + "/dist",
+    },
+    module: {
+        rules: [
+          {
+            test: /\.jpg$/i,
+            use: [
+                {
+                  // npm i -D file-loader
+                  loader: 'file-loader',
+                  options: {
+                    esModule: false,
+                    name(file) {
+                      return '[path][name].[ext]';
+                    },
+                    publicPath: function(url) {
+                      return url.replace('../', '/assets/');
+                    }
+                  }
+                },
+                { 
+                  // npm i image-webpack-loader
+                  loader: 'image-webpack-loader'
+                }
+            ]
+          }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery"
+        }),
+        new BundleAnalyzerPlugin({
+        analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+        })
+    ],
+    mode: "development"
 };
